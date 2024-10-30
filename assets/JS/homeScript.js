acd90894203dd09288f59a5fbacf8384009c7643
unknown <masalmanouran@gmail.com>
2024-10-30 19:00:31 +0300

    add darck mode

--- assets/JS/homeScript.js
+++ assets/JS/homeScript.js
@@ -9,3 +9,19 @@ document.querySelector(".close").onclick = () =>
 {
     loginForm.classList.remove('active');
 }
+
+let body=document.querySelector("body");
+let darck=document.querySelector(".darck");
+let darckBtn=document.querySelector(".darckBtn");
+function toDarck(){
+
+    const buttonImage = document.getElementById("buttonImage");
+    const image1 = "assets/images/icons/moon.png"; 
+     const image2 = "assets/images/icons/sun.png"; 
+     buttonImage.src = buttonImage.src.includes(image1) ? image2 : image1;
+
+    body.classList.toggle("darck");
+
+   
+}
+darckBtn.onclick=toDarck;
